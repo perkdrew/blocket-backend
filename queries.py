7cb0176e15d56608284994e8e8f4d6c383fb2f51
drewmaniva <drew@manivadigital.com>
2021-08-14 10:41:24 +0200

    small refactor of resolver methods

--- queries.py
+++ queries.py
@@ -11,7 +11,8 @@ class Query(graphene.ObjectType):
     all_advertisements = graphene.List(AdvertisementModel, sort=graphene.Int(required=False))
     advertisement_by_id = graphene.Field(AdvertisementModel, advertisement_id=graphene.Int(required=True))
 
-    def resolve_all_advertisements(self, info: dict, sort: SortEnum = None):
+    @staticmethod
+    def resolve_all_advertisements(root, info: dict, sort: SortEnum = None):
         if sort == SortEnum.created_at:
             return db.query(models.Advertisement).order_by(models.Advertisement.created_at).all()
         elif sort == SortEnum.price:
@@ -19,5 +20,6 @@ class Query(graphene.ObjectType):
         else:
             return db.query(models.Advertisement).all()
 
-    def resolve_advertisement_by_id(self, info: dict, advertisement_id: int):
+    @staticmethod
+    def resolve_advertisement_by_id(root, info: dict, advertisement_id: int):
         return db.query(models.Advertisement).filter(models.Advertisement.id == advertisement_id).first()
